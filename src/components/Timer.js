e57e79c97ae19596cf03b9ff8eab7dacac9f6649
Matthieu <matthieunadeau96@gmail.com>
2018-09-01 11:51:11 -0400

    cleaned up a bit

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -13,38 +13,8 @@ class Timer extends Component {
     seconds: 26
   }
 
-  secondsToTime = (secs) => {
-    let hours = Math.floor(secs / (60 * 60));
-
-    let divisor_for_minutes = secs % (60 * 60);
-    let minutes = Math.floor(divisor_for_minutes / 60);
-
-    let divisor_for_seconds = divisor_for_minutes % 60;
-    let seconds = Math.ceil(divisor_for_seconds);
-
-    if (seconds < 10) {
-      seconds = '0' + seconds;
-    }
-
-    let obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
-    };
-    return obj;
-  }
-
-  componentDidMount = () => {
-    this.resetDisplay(this.props.workTimer);
-  }
-
-  resetDisplay = (secs) => {
-    let timeLeftVar = this.secondsToTime(secs);
-    this.setState({ time: timeLeftVar });
-  }
-
   render() {
-    const {count, counting, progressBarCountdown, mode} = this.state
+    const {counting, progressBarCountdown, mode} = this.state
 
     return (
       <div className="timerContainer">
@@ -77,8 +47,36 @@ class Timer extends Component {
     );
   }
 
-  // TODO: I have to fix the way startTimer interacts with pauseTimer
-    // as of right now when I press play after pausing, the timer resets back to what is set inside the if conditions below
+  componentDidMount = () => {
+    this.resetDisplay(this.props.workTimer);
+  }
+
+  resetDisplay = (secs) => {
+    let timeLeftVar = this.secondsToTime(secs);
+    this.setState({ time: timeLeftVar });
+  }
+
+  secondsToTime = (secs) => {
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+    return obj;
+  }
+
 
   countDown = () => {
     const { workTimer, breakTimer, bigBreakTimer } = this.props
@@ -107,7 +105,6 @@ class Timer extends Component {
   }
 
   startTimer = () => {
-    const { workTimer, breakTimer, bigBreakTimer } = this.props
     const { mode } = this.state
 
     this.setState({counting: true})
@@ -121,7 +118,6 @@ class Timer extends Component {
     else if (mode === 'BIGBREAK') {
       this.myInterval = setInterval(this.countDown, 1000);
     }
-
     this.timerCountDown()
   }
 
@@ -137,34 +133,24 @@ class Timer extends Component {
       return
     }
     this.pauseTimer()
-
     this.setState({count: setTimer, progressBarCountdown: 100})
-
   }
 
   skipTimer = () => {
     const {workTimer, breakTimer, bigBreakTimer} = this.props
     const { mode } = this.state
-    console.log(mode)
-    console.log(this.state.seconds)
 
 
     if (mode === 'WORK') {
       this.setState({ mode: 'BREAK', seconds: breakTimer, progressBarCountdown: 100 })
-      console.log(mode)
-      console.log(this.state.seconds)
       this.resetDisplay(breakTimer);
     }
     if (mode === 'BREAK') {
       this.setState({ mode: 'BIGBREAK', seconds: bigBreakTimer, progressBarCountdown: 100 })
-      console.log(mode)
-      console.log(this.state.seconds)
       this.resetDisplay(bigBreakTimer);
     }
     if (mode === 'BIGBREAK') {
       this.setState({ mode: 'WORK', seconds: workTimer, progressBarCountdown: 100 })
-      console.log(mode)
-      console.log(this.state.seconds)
       this.resetDisplay(workTimer);
     }
 
